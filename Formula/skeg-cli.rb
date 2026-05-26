f07bd7ce0f208563962b7e902e5e57189d035fe6
Daniele <589793+lupodevelop@users.noreply.github.com>
2026-05-26 11:29:23 +0200

    skeg-cli 0.1.1: bump aarch64-apple-darwin sha256

--- Formula/skeg-cli.rb
+++ Formula/skeg-cli.rb
@@ -1,7 +1,7 @@
 class SkegCli < Formula
   desc "Operator tools for skeg (build, inspect, stats)"
   homepage "https://github.com/skegdb/skeg-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache-2.0"
 
   # aarch64 only. skeg-cli links skeg-vector, whose distance kernels are
@@ -10,8 +10,8 @@ class SkegCli < Formula
   depends_on arch: :arm64
 
   if OS.mac?
-    url "https://github.com/skegdb/skeg-cli/releases/download/v#{version}/skeg-cli-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "ed6b4372f28c87885f52448589f47dcd17cbbc8d2fe5745595a625ff5d396ecd"
+    url "https://github.com/skegdb/skeg-cli/releases/download/v0.1.1/skeg-cli-v0.1.1-aarch64-apple-darwin.tar.gz"
+    sha256 "ae24fdd06aef424f4d1630b3625f9ac41e93f58b033f039ee2b064e07cab2c46"
   elsif OS.linux?
     url "https://github.com/skegdb/skeg-cli/releases/download/v#{version}/skeg-cli-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
     sha256 "5f7a67e0fc37f017c1a5c0ad4a95f8dba999e75518b6d9b2b70d436b314e37bd"
